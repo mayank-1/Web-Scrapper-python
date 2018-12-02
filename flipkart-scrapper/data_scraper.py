6533281cd21a8c364cb56b3cb2ae1b2460aedb55
Mayank Kumar <mayankmk165@gmail.com>
2018-12-02 21:55:06 +0530

    Final Upload
    Data Scrapping for Flipkart site is done. And also exported the data in CSV file.

--- flipkart-scrapper/data_scraper.py
+++ flipkart-scrapper/data_scraper.py
@@ -1,24 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+from array import array
 
 source = requests.get('https://www.flipkart.com/search?q=phone&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off').content
 
 soup = BeautifulSoup(source, 'html.parser')
 
-# For Single Data
-for article in soup.find('div'):
+csv_file = open('flipkart_scrape.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Product Name','Price','Specifications'])
 
-    headline = article.find('div', class_='_3wU53n').text
+for article in soup.find_all('div',class_='_1UoZlX'):
+    headline = article.find('div',class_='_3wU53n').text
     print(headline)
 
     price = article.find('div',class_='_1vC4OE').text
     print(f'Price: {price}')
 
-    print('Specifications:-')
-    for detail in article.find('ul', class_='vFw0gD').find_all('li', class_='tVe95H'):
-        print(detail.text)
+    print('Specification:-')
+    # for detail in article.find('ul',class_='vFw0gD').find_all('li',class_='tVe95H'):
+    #     specs = detail.text
+    #     print(specs)
+
+    specs=[]
+    for detail in article.find('ul',class_='vFw0gD').find_all('li',class_='tVe95H'):
+        specs.append(detail.text)
         
+    final_detail = ', '.join(specs)
+    print(final_detail)
+
+
     print()
 
-# For multiple data
-#TODO:-
+    csv_writer.writerow([headline,price,final_detail])
+
+csv_file.close()
